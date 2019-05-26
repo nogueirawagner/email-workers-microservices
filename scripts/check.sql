c4086b31445014afb65c4725fd686b0f4b5e6323
Wagner Nogueira <engnogueirawgn@hotmail.com>
2019-05-26 11:25:20 -0300

    criando volumes e scripts de inicializaçao

--- scripts/check.sql
+++ scripts/check.sql
@@ -1,6 +1,3 @@
-\l -- listar todos os banco de dados cadastrados, agora já é para o email_sender aparecer na lista
-\c email_sender -- se conectar na base
-
--- mostrar uma descrição dessa tabela de emails para mostrar as 
--- colunas e verificar que o script de inicialização executou de forma bem sucedida.
+\l
+\c email_sender 
 \d emails 
--- scripts/init.sql
+++ scripts/init.sql
@@ -1,11 +1,10 @@
-create database email_sender;
+create database email_sender
 
--- conectar com o banco de dados
 \c email_sender 
 
 create table emails (
     id serial not null,
-    data timestamp not null default current_timestamp,
+    datacriacao timestamp not null default current_timestamp,
     assunto varchar(100) not null, 
     mensagem varchar(250) not null,
 );
